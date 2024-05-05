a6179ea89746c399f937d28c446ac2284c8f089e
NikoEWE <nikoelgamerprah@gmail.com>
2024-05-05 00:58:22 -0600

    responds with a joke

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
+
 export async function POST(REQUEST){
     const req = await REQUEST.json()
+
     
-    let res
-    fetch('https://v2.jokeapi.dev/joke/any')
-    .then(x => x.json())
-    .then(x => res = x)
-    console.log(res)
-    
-    return NextResponse.json({context: res})
+    let raw = await fetch('https://v2.jokeapi.dev/joke/any')
+    let json = await raw.json()
+    let res = await json
+    console.log(json)
+    return NextResponse.json({context: json})
 }
