c1321331753718bec56fa6b89c53d2c1ccfb47c3
NikoEWE <nikoelgamerprah@gmail.com>
2024-05-05 15:19:16 -0600

    asd

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req){
     req = await req.json()
-
+    console.log(req)
     let category = req.context['4ent']
     let raw = await fetch(`https://v2.jokeapi.dev/joke/${category}`)
     let json = await raw.json()
@@ -13,17 +13,18 @@ export async function POST(req){
 
     let jokeText = {
         type: res.type,
-        line1: res.type === 'single' ? res.joke : res.setup,
-        line2: res.type === 'twopart' ? res.delivery : ''
+        line1: (res.type === 'single') ? res.joke : res.setup,
+        line2: (res.type === 'twopart') ? res.delivery : ''
     }
+    console.log(res)
 
     return NextResponse.json(
         {
-        responses: [
+        responses: 
             {
             type: 'text',
             texts: [jokeText.line1],
-        }],
+        },
         context: {
             type: jokeText.type,
             line2: jokeText.line2
