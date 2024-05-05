e5c5e4c05ca3cfd8be45ea0864d5b86337d2f46a
NikoEWE <nikoelgamerprah@gmail.com>
2024-05-05 13:59:07 -0600

    1.01

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,22 @@ import { useEffect, useState } from "react"
 export default function Home() {
 
   const [loaded, setLoaded] = useState(false)
-  const openChat = async () =>{
-    window.MoveoAI.init({ 
-      integrationId: "6d9b0f7c-95d2-44ba-822a-f065e4692900",
-      element:document.getElementById('embed')})
-    .then((desk) => console.log("Moveo connected"))
-    .catch((error) => console.error(error)
-    );
-  }
   useEffect(()=>{
-    openChat()
+    
   },[loaded])
+
   useEffect(()=>{
     const scriptTag = document.createElement('script');
     scriptTag.src =
       'https://cdn.jsdelivr.net/npm/@moveo-ai/web-client@latest/dist/web-client.min.js';
     scriptTag.addEventListener('load', () => {
       console.log('Script is loaded');
-      setLoaded(true)
+      window.MoveoAI.init({ 
+        integrationId: "6d9b0f7c-95d2-44ba-822a-f065e4692900",
+        element:document.getElementById('embed')})
+      .then((desk) => console.log("Moveo connected"))
+      .catch((error) => console.error(error)
+      );
     });
     document.body.appendChild(scriptTag);
     
