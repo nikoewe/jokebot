40f04de480783e9566c459a3b055505827e45eb4
NikoEWE <nikoelgamerprah@gmail.com>
2024-05-05 00:49:39 -0600

    fix

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,8 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function POST(REQUEST){
-
     const req = await REQUEST.json()
-    console.log(req)
-    return NextResponse.json(req)
+    
+    let res
+    fetch('https://v2.jokeapi.dev/joke/any')
+    .then(x => x.json())
+    .then(x => res = x)
+    console.log(res)
+    
+    return NextResponse.json({context: res})
 }
