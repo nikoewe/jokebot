03bd8728eb56fc4864a11d38cde450b88afc9f64
NikoEWE <nikoelgamerprah@gmail.com>
2024-05-06 11:22:22 -0600

    code refactoring

--- src/app/page.js
+++ src/app/page.js
@@ -2,31 +2,15 @@
 
 import { useEffect, useState } from "react"
 
+import chatBoxLoader from "./utils/chatBoxLoader"
+
 
 export default function Home() {
 
-  const [loaded, setLoaded] = useState(false)
-  useEffect(()=>{
-    
-  },[loaded])
-
-  useEffect(()=>{
-    const scriptTag = document.createElement('script');
-    scriptTag.src =
-      'https://cdn.jsdelivr.net/npm/@moveo-ai/web-client@latest/dist/web-client.min.js';
-    scriptTag.addEventListener('load', () => {
-      console.log('Script is loaded');
-      window.MoveoAI.init({ 
-        integrationId: "6d9b0f7c-95d2-44ba-822a-f065e4692900",
-        element:document.getElementById('embed')})
-      .then((desk) => console.log("Moveo connected"))
-      .catch((error) => console.error(error)
-      );
-    });
-    document.body.appendChild(scriptTag);
-    
-
   
+
+  useEffect(()=>{
+    chatBoxLoader()
   },[])
 
   return (
---
+++ src/app/utils/chatBoxLoader.js
@@ -0,0 +1,16 @@
+export default function chatBoxLoader (){
+    const scriptTag = document.createElement('script');
+    scriptTag.src =
+      'https://cdn.jsdelivr.net/npm/@moveo-ai/web-client@latest/dist/web-client.min.js';
+    scriptTag.addEventListener('load', () => {
+      console.log('Script is loaded');
+      window.MoveoAI.init({ 
+        integrationId: "6d9b0f7c-95d2-44ba-822a-f065e4692900",
+        element:document.getElementById('embed')})
+      .then((desk) => console.log("Moveo connected"))
+      .catch((error) => console.error(error)
+      );
+    });
+    document.body.appendChild(scriptTag);
+
+}
