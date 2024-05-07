e7a01a44b21ed3cf10d97a999ecbdcc18ceb050c
NikoEWE <nikoelgamerprah@gmail.com>
2024-05-07 10:35:03 -0600

    beta 1.0

--- src/app/components/Arcade.js
+++ src/app/components/Arcade.js
@@ -1,8 +1,9 @@
 import './arcade.css'
+import bmo from '../bmo.png'
 export default function Arcade({...props}){
 
     return(
-        <div>
+        <div className='background'>
             <div className='screen' id='embed'></div>
         </div>
     )
